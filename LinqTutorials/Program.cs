18b31e8620f8cc319378b21d5fe175c783742b5a
Berkay <wberkayw@gmail.com>
2024-05-14 11:09:54 +0200

    Tasks done.

---
+++ LinqTutorials/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LinqTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = LinqTasks.Task1();
+            var t2= LinqTasks.Task2();
+            var t4 = LinqTasks.Task4();
+            var t5 = LinqTasks.Task5();
+            var t6 = LinqTasks.Task6();
+            var t7 = LinqTasks.Task7();
+            var t8 = LinqTasks.Task8();
+            var t9 = LinqTasks.Task9();
+            var t10 = LinqTasks.Task10();
+            var t11 = LinqTasks.Task11();
+            var t12 = LinqTasks.Task12();
+            int[] arr = new[] {1,1,1,1,1,1,10,1,1,1,1};
+            var t13 = LinqTasks.Task13(arr);
+            var t14 = LinqTasks.Task14();
+            var t15 = LinqTasks.Task15();
+            var t16 = LinqTasks.Task16();
+            LinqTasks.ShowTask3Result();
+        }
+    }
+}
